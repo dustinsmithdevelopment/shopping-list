33fa2d799b92e9719fc3e86bfe5d80a6754c6659
Dustin Smith <dustinsmithdevelopment@gmail.com>
2024-05-06 21:32:10 -0500

    Adding empty modules

---
+++ src/app/shopping-list/shopping-list-editor/shopping-list-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-editor',
+  templateUrl: './shopping-list-editor.component.html',
+  styleUrl: './shopping-list-editor.component.css'
+})
+export class ShoppingListEditorComponent {
+
+}
